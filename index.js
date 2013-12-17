09fbfd478773d524148f5620a0233fb07d55d4f0
zeMirco <mirco.zeiss@gmail.com>
2013-12-17 17:08:03 +0100

    used short fn notation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-var fs = require('fs');
-var path = require('path');
-var ejs = require('ejs');
+var readFile = require('fs').readFile;
+var join = require('path').join;
+var render = require('ejs').render;
 
 module.exports = function(title, content, cb) {
   
   // read html file
-  fs.readFile(path.join(__dirname, 'index.html'), 'utf8', function(err, data) {
+  readFile(join(__dirname, 'index.html'), 'utf8', function(err, data) {
     if (err) return cb(err);
     
     // set local variables
@@ -16,7 +16,7 @@ module.exports = function(title, content, cb) {
     };
     
     // create html
-    var html = ejs.render(data, locals);
+    var html = render(data, locals);
     
     // return modified html content
     cb(null, html);
